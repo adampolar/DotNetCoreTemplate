b14beb43c1648045a1101169a5f81d05b510cd63
= <=>
2017-03-03 21:31:36 +0000

    dotnetcore template for command line app with test app

---
+++ src/ProjectName/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Add(2,2));
+        }
+        
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        } 
+    }
+}
---
+++ test/ProjectName.Tests/Tests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace Tests
+{
+    
+    public class Tests
+    {
+        [Fact]
+        public void TwoAndTwoIsFour() 
+        {
+            Assert.Equal(ConsoleApplication.Program.Add(2,2), 4);
+        }
+    }
+}
